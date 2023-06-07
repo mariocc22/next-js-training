845e0fcacbd732747b897d92cbef1773ee528a61
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-06-06 23:58:45 -0700

    adding posts and about routes

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Post = ({params}) => {
-  // estos params vienen de la URL! sin tener que pasar por props
+  // estos params vienen de la URL! sin tener que pasar por props, asi es con todas las rutas anidadas
   const {id} = params;
   return (
     <h1>Esto es el post {id}</h1>
